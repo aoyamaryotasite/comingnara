0038e0789f6e71457764305735008de5628bfd4d
aoyamaryotasite <68090032+aoyamaryotasite@users.noreply.github.com>
2020-07-28 19:32:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// 文字を浮かび上がらせる
+$(function(){
+  $(window).scroll(function (){
+    $('.fadein').each(function(){
+      var elemPos = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 200){
+        $(this).addClass('scrollin');
+      }
+    });
+  });
+});
+
+
+$(function () {
+  // ナビゲーションをクリック
+  $("a[href*=#]:not([href=#])").click(function () {
+    // 移動先のコンテンツの位置を取得
+    var target = $($(this).attr("href")).offset().top;
+    // 70px減らす
+    target -= 70;
+    // コンテンツへスクロール
+    $("html, body").animate({ scrollTop: target }, 500);
+    return false;
+  });
+});
